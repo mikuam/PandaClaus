b2999e5cce5698d99761a5179aef8a3babfe00ab
Michał Białecki <michal.bialecki@guestline.com>
2023-10-02 09:52:02 +0200

    Dodanie strony listu

---
+++ PandaClaus.Web/Pages/Letter.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PandaClaus.Web.Pages;
+public class LetterModel : PageModel
+{
+    private readonly GoogleSheetsClient _client;
+
+    public Letter Letter { get; set; }
+
+    public LetterModel(GoogleSheetsClient client)
+    {
+        _client = client;
+    }
+
+    public async Task OnGetAsync(int rowNumber)
+    {
+        Letter = await _client.FetchLetter(rowNumber);
+    }
+}
+
