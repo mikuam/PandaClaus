fd0d16c6c2c5e64f99d9b3677f29c09da3a507cf
Michał Białecki <michal.bialecki@guestline.com>
2023-11-20 19:46:14 +0100

    Added an admin parameter

--- PandaClaus.Web/Pages/Index.cshtml.cs
+++ PandaClaus.Web/Pages/Index.cshtml.cs
@@ -3,6 +3,8 @@
 namespace PandaClaus.Web.Pages;
 public class IndexModel : PageModel
 {
+    private const string AdminParameter = "isAdminAccess";
+
     private readonly GoogleSheetsClient _client;
     public List<Letter> Letters { get; set; } = new();
 
@@ -13,7 +15,15 @@ public class IndexModel : PageModel
 
     public async Task OnGetAsync()
     {
-        var result = (await _client.FetchLetters()).Where(l => l.IsVisible).ToList();
+        var isAdmin = Request.Query.ContainsKey(AdminParameter) && Request.Query[AdminParameter] == "true";
+
+        var result = (await _client.FetchLetters()).ToList();
+
+        if (!isAdmin)
+        {
+            result = result.Where(l => l.IsVisible).ToList();
+        }
+
         result.ForEach(l => l.Description = TrimLength(l.Description));
         Letters = result;
     }
