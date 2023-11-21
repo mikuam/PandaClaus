c21b6e10b922c2abe97bcd58ddcc9604c491f574
Michał Białecki <michal.bialecki@guestline.com>
2023-11-21 14:59:27 +0100

    Added sorting by numbers and filtering

---
+++ PandaClaus.Web/CustomComparer.cs
@@ -0,0 +1,30 @@
+﻿namespace PandaClaus.Web;
+
+class CustomComparer : IComparer<string>
+{
+    private const string Unknown = "X";
+
+    public int Compare(string x, string y)
+    {
+        if (x == Unknown &&  y == Unknown) return 0;
+        if (x == Unknown) return 1;
+        if (y == Unknown) return -1;
+
+        // Extract numeric and non-numeric parts
+        string[] xParts = System.Text.RegularExpressions.Regex.Split(x, @"([0-9]+)");
+        string[] yParts = System.Text.RegularExpressions.Regex.Split(y, @"([0-9]+)");
+
+        // Compare numeric parts as integers
+        int xNumeric = int.Parse(xParts[1]);
+        int yNumeric = int.Parse(yParts[1]);
+        int numericComparison = xNumeric.CompareTo(yNumeric);
+
+        if (numericComparison != 0)
+        {
+            return numericComparison;
+        }
+
+        // If numeric parts are equal, compare non-numeric parts lexicographically
+        return string.Compare(xParts[2], yParts[2]);
+    }
+}
--- PandaClaus.Web/Pages/Index.cshtml.cs
+++ PandaClaus.Web/Pages/Index.cshtml.cs
@@ -3,21 +3,55 @@
 namespace PandaClaus.Web.Pages;
 public class IndexModel : PageModel
 {
-    private const string AdminParameter = "isAdminAccess";
+    private const string AdminParameter = "adminCode";
+    private const string ActiveButton = "btn-primary";
+    private const string InActiveButton = "btn-outline-primary";
 
     private readonly GoogleSheetsClient _client;
-    public List<Letter> Letters { get; set; } = new();
+    private readonly string _adminCode;
 
-    public IndexModel(GoogleSheetsClient client)
+    public List<Letter> Letters { get; set; } = new();
+    public string ButtonFiltersAll = "btn-primary";
+    public string ButtonFiltersReserved = "btn-outline-primary";
+    public string ButtonFiltersAvailable = "btn-outline-primary";
+
+    public IndexModel(GoogleSheetsClient client, IConfiguration configuration)
     {
         _client = client;
+        _adminCode = configuration["AdminCode"]!;
     }
 
     public async Task OnGetAsync()
     {
-        var isAdmin = Request.Query.ContainsKey(AdminParameter) && Request.Query[AdminParameter] == "true";
+        var isAdmin = Request.Query.ContainsKey(AdminParameter) && Request.Query[AdminParameter] == _adminCode;
 
         var result = (await _client.FetchLetters()).ToList();
+        var filter = Request.Query["filter"].ToString();
+        switch (filter)
+        {
+            case "available":
+                result = result.Where(l => !l.IsAssigned).ToList();
+
+                ButtonFiltersAll = InActiveButton;
+                ButtonFiltersReserved = InActiveButton;
+                ButtonFiltersAvailable = ActiveButton;
+
+                break;
+            case "reserved":
+                result = result.Where(l => l.IsAssigned).ToList();
+
+                ButtonFiltersAll = InActiveButton;
+                ButtonFiltersReserved = ActiveButton;
+                ButtonFiltersAvailable = InActiveButton;
+
+                break;
+            default:
+
+                ButtonFiltersAll = ActiveButton;
+                ButtonFiltersReserved = InActiveButton;
+                ButtonFiltersAvailable = InActiveButton;
+                break;
+        }
 
         if (!isAdmin)
         {
@@ -25,7 +59,7 @@ public class IndexModel : PageModel
         }
 
         result.ForEach(l => l.Description = TrimLength(l.Description));
-        Letters = result;
+        Letters = result.OrderBy(l => l.Number, new CustomComparer()).ToList();
     }
 
     private string TrimLength(string text)
