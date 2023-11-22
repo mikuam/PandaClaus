3281d0e99d0aa311b9fa13f13441813c51486bc9
Michał Białecki <michal.bialecki@guestline.com>
2023-11-22 10:15:38 +0100

    Updated email confirmation

--- PandaClaus.Web/EmailSender.cs
+++ PandaClaus.Web/EmailSender.cs
@@ -24,14 +24,28 @@ public class EmailSender
         var subject = "Potwierdzenie rezerwacji listu oraz ważne informacje";
         var plainTextContent = $"Cześć {letter.AssignedTo}.\n\n" +
                                $"Dziękujemy za to, że chcesz zostać PANDAstycznym Mikołajem :)! Potwierdzamy realizowanie wybranego przez Ciebie listu nr {letter.Number}" +
-                               $"Potwierdzamy przypisanie listu do Ciebie. " +
-                               $"Oto link do listu: {GetLetterUrl(letter)} " +
-                               $"W razie pytań prosimy o kontakt na adres e-mail pandaclaus@pandateam.pl.\n\n" +
-                               $"Pozdrawiamy,\n" +
-                               $"Zespół Panda Team";
+                               $"Poniżej garść WAŻNYCH informacji:\r\n" +
+                               $"Czas na realizację i dostarczenie do nas paczki jest do 8 grudnia 2023.\r\n" +
+                               $"Masz dwie możliwości dostarczenia paczki:\r\n" +
+                               $"Paczkomatem Inpost do Paczkomatu POZ174M podając e-mail pandaclaus@pandateam.pl oraz numer telefonu +48 534 897 105 (jeżeli paczka nie zmieści się do Paczkomatu Inpost - daj nam znać na wyżej podany adres mailowy)\r\n" +
+                               $"Osobiście do siedziby naszej fundacji Hope Spot na ul. Bolka 8A w Poznaniu w dniach: 5-7 grudnia od 10:30 do 14:30 oraz 8 grudnia od 16:30 do 19:00 (inne dni i godziny po wcześniejszym umówieniu telefonicznym)\r\n" +
+                               $"WAŻNE! Prosimy Cię o wyraźne oznaczenie paczki numerami listów, najlepiej w kilku miejscach.\r\n" +
+                               $"Jeżeli realizujesz kilka listów, paczkę dla każdego dziecka spakuj osobno i umieść wszystkie możliwie w jednym kartonie.\r\n" +
+                               $"Jeżeli to możliwe, prosimy, aby największa paczka nie przekraczała wymiarów 41x38x64 cm (choć zdajemy sobie sprawę, że nie zawsze da się to zrealizować)\r\n\r\n" +
+                               $"W dniu finału akcji będziemy otwierać każdą z paczek i przygotowywać do dalszej wysyłki. Nie musisz więc pakować jej w ozdobny papier, po prostu wyraźnie oddzielcie prezenty dla każdego z dzieci i podpisz numerem listu na zewnętrznej stronie.\r\n\r\n" +
+                               $"Wartość prezentu zapisanego w liście dla jednego dziecka nie powinna przekraczać 300 zł. Jeżeli jest inaczej i nie możesz zrealizować przez to listu, koniecznie daj nam znać pisząc na adres pandaclaus@pandateam.pl.\r\n\r\n" +
+                               $"W liście podane jest główne marzenie dziecka. Jeżeli jednak chcesz dorzucić coś od siebie, to śmiało możesz to zrobić. Jeśli dziecko pisze o zabawce z np. z Psiego Patrolu, to można mu dodatkowo kupić jakieś małe gadżety związane z prezentem głównym np. naklejki, gazetki itp. \r\n\r\n" +
+                               $"Do paczek koniecznie dołączcie kartki z życzeniami albo krótki spersonalizowany list. Prosimy, aby podpisać je np. „Święty Mikołaj z pomocnikami” i wymienić Wasze imiona/nazwę firmy, czy klasy szkolnej/grupy przedszkolnej. Wiele dzieci wierzy mocno, że to idzie prosto od świętego Mikołaja, ale zarazem chcielibyśmy, aby rodzice wiedzieli, kto był Darczyńcą. Jeśli bardzo chcecie pozostać anonimowi to możecie napisać ogólnikowo np. Święty Mikołaj z pomocnikami z firmy produkującej XYZ, z branży XYZ lub szkoły w XYZ. \r\n\r\n" +
+                               $"Będzie wspaniale, jeśli dorzucisz jakiś drobny upominek dla rodziców (kawa, herbata, słodycze, damskie i męskie skarpetki, kubki świąteczne, świece itp.). Rodzice naszych podopiecznych często stawiają siebie i swoje potrzeby na samym końcu… na pewno będzie im miło. \r\n\r\n" +
+                               $"W razie pytań, pisz na pandaclaus@pandateam.pl lub dzwoń na numer +48 534 897 105.\r\n\r\n" +
+                               $"I ostatnia prośba. Wysyłajcie nam swoje zdjęcia z realizacji prezentów, a wrzucając je w social media oznaczajcie nas @fundacjapandateam.\r\n\r\n" +
+                               $"Jeszcze raz BARDZO dziękujemy za Wasze dobre serca!\r\n\r\n" +
+                               $"Wszystkiego PANDAstycznego!\r\n\r\n" +
+                               $"Ekipa z Panda Team\r\n";
 
         var htmlContent = $"Cześć {letter.AssignedTo}.<br />" +
                           $"<p>Dziękujemy za to, że chcesz zostać PANDAstycznym Mikołajem :)! Potwierdzamy realizowanie wybranego przez Ciebie listu nr {letter.Number}.</p>" +
+                          $"<p>List znajdziesz pod tym adresem: <a href=\"{GetLetterUrl(letter)}\">{GetLetterUrl(letter)}</a></p>" +
                           "<p>Poniżej garść WAŻNYCH informacji:</p>" +
                           "<ul>" +
                           "<li>Czas na realizację i dostarczenie do nas paczki jest do 8 grudnia 2023.</li>" +
