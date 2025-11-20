6920c6abfd8a4063fbcd60eccce9208409ce423d
Michał Białecki <michal.bialecki@dotdigital.com>
2025-11-20 11:16:56 +0100

    Updated e-mail content

--- PandaClaus.Web/Core/EmailSender.cs
+++ PandaClaus.Web/Core/EmailSender.cs
@@ -30,48 +30,79 @@ public class EmailSender
 
         var subject = "Potwierdzenie rezerwacji listu oraz ważne informacje";
         var plainTextContent = $@"Drodzy PANDAstyczni Darczyńcy!
-Bardzo dziękujemy za Wasze zaangażowanie w akcję Panda Claus 
+Bardzo dziękujemy za Wasze zaangażowanie w akcję Panda Claus 2025.
 Gratulujemy {letter.AssignedTo}! Potwierdzamy realizowanie wybranego przez Ciebie listu nr {letter.Number}. List znajdziesz pod tym adresem: {GetLetterUrl(letter)}
-Jak wiecie, w tym roku mieliśmy rekordową liczbę zgłoszonych listów — dokładnie 240! Akcja z roku na rok rośnie i jest coraz większym wyzwaniem logistycznym. W związku z tym przypominamy najważniejsze zasady i dziękujemy Wam za wyrozumiałość.
-Paczki muszą być oznaczone numerami listów — to BARDZO ważne i pozwoli nam uniknąć pomyłek.
-Proszę o wysyłanie skompletowanych paczek. Tylko w taki sposób jesteśmy w stanie uniknąć pomyłek. Bardzo złym pomysłem jest zamawianie zabawek bezpośrednio ze sklepu do nas i dosyłanie osobno innych upominków. To prowadzi do błędów i innych trudności organizacyjnych. Takie paczki nie będą niestety przyjmowane.
-Jeżeli realizujesz kilka listów, paczkę dla każdego dziecka spakuj osobno i umieść wszystkie możliwie w jednym kartonie.
-Do listu możesz dołączyć upominek dla rodziców dziecka, list lub kartkę oraz słodycze itd.
-W tym roku naszym Partnerem jest InPost, dlatego paczki muszą mieć rozmiary zgodne z wymaganiami Paczkomatów InPost.
-Paczkę wyślij Paczkomatem InPost pod numer POZ82M.
-(jeżeli paczka nie zmieści się do Paczkomatu Inpost — daj nam znać na wyżej podany adres mailowy. Prosimy, nie nadawaj paczki kurierem bez informowania nas o tym)
-Nadając przesyłkę podaj e-mail pandaclaus@pandateam.pl oraz numer telefonu +48 534 897 105
+Mamy absolutny rekord - jak wiecie, w tym roku - trafiło do nas ponad 300 listów! To trzysta dziecięcych marzeń, trzysta historii, trzysta prezentów czekających na spełnienie.
+Akcja z roku na rok rośnie i jest coraz większym wyzwaniem logistycznym. W związku z tym przypominamy najważniejsze zasady i dziękujemy Wam za wyrozumiałość:
+- Na paczki czekamy do 6 grudnia!
+- Paczki muszą być oznaczone numerami listów — to BARDZO ważne i pozwoli nam uniknąć pomyłek. 
+- Prosimy Was także o wydrukowanie (z załącznika) kodu QR i przyklejenie go na paczce - ułatwi to naszym Magazynowym Elfom pracę i dzięki temu dostaniesz od nas POTWIERDZENIE ODBIORU PACZKI!
+- Prosimy o wysyłanie skompletowanych paczek. Tylko w taki sposób jesteśmy w stanie uniknąć pomyłek. Prosimy nie zamawiać zabawek bezpośrednio ze sklepu do nas i dosyłanie osobno innych upominków. To prowadzi do błędów i innych trudności organizacyjnych. Takie paczki nie będą niestety przyjmowane.
+- Prosimy nie pakować paczek w ozdobny papier - podczas finału akcji nasi wolontariusze sprawdzają każdą z nich i szykują prezenty do wysyłki :).
+- Zapraszamy do skorzystania z zasobów naszego Partnera - Biblioart - zachęcamy do sprawdzenia strony www - może to właśnie tam znajdziecie wymarzony prezent naszych podopiecznych: https://bibliopunkt.pl/
+- Jeżeli realizujesz kilka listów, paczkę dla każdego dziecka spakuj osobno i umieść wszystkie możliwie w jednym kartonie - opisując paczkę każdym z numerów listów, które realizujesz!
+- Do listu możesz dołączyć upominek dla rodziców dziecka, list lub kartkę oraz słodycze itd.
+
+Jak co roku naszym Partnerem jest InPost, dlatego paczki muszą mieć rozmiary zgodne z wymaganiami Paczkomatów InPost - Elfowie Kurierzy będą pracować w pocie czoła - pomóżmy im i trzymajmy się wymiarów paczkomatowych - mała ściąga tutaj - 
+Gabaryt A – maksymalne wymiary 8 × 38 × 64 cm, maksymalna waga: 25 kg 
+Gabaryt B – maksymalne wymiary 19 × 38 × 64 cm, maksymalna waga: 25 kg
+Gabaryt C – maksymalne wymiary 41 × 38 × 64 cm, maksymalna waga: 25 kg
+
+Paczkę wyślij do nas pod adres:
+Fundacja Panda Team
+ul. Opłotki 23,
+60-012 Poznań
+Nadając przesyłkę, podaj e-mail: pandaclaus@pandateam.pl oraz numer telefonu: +48 537 925 335.
+
 Jeżeli chcesz dostarczyć paczkę osobiście:
-Przywieź paczkę do naszego magazynu na ul. Głogowskiej 16 w Poznaniu (naprzeciwko Dworca Zachodniego, hala numer 2 MTP) w dniach:
-- 5-6 grudnia od 17:30 do 20:30
-- 7 grudnia od 10:30 do 14:30
+Poinformuj nas o tym do 6 grudnia!
+Przywieź paczkę do naszego magazynu w Poznaniu Hala numer 10 MTP (wejście od ulicy Śniadeckich) w dniu:
+11 grudnia 2025 od godziny 16:00 do 20:30
+
 Z góry BARDZO dziękujemy za dostosowanie się do naszych szczegółowych wytycznych. Pomoże nam to sprawnie przeprowadzić akcję i dostarczyć prezenty do naszych podopiecznych :)
-Już teraz zapraszamy Was do zgłaszania się do wolontariatu podczas finału, który odbędzie się 13 i 14 grudnia (pt-sob) w hali nr 10 Międzynarodowych Targów Poznańskich. Chęć pomocy można zgłosić poprzez formularz dostępny na stronie https://forms.gle/xn8UMCst3uSTwoeV9 (wolontariaty pracownicze proszę zgłaszać e-mailowo). Będzie nam miło, jak pomożecie nam w finale! :) 
+Już teraz zapraszamy Was do zgłaszania się do wolontariatu podczas finału, który odbędzie się 12 i 13 grudnia (pt-sob) w hali nr 10 Międzynarodowych Targów Poznańskich. Chęć pomocy można zgłosić poprzez wysłanie wiadomości e-mail na adres: wolontariat@pandateam.pl (wolontariaty pracownicze także prosimy zgłaszać e-mailowo). Będzie nam miło, jak pomożecie nam w finale! :)
 Z całym #pandateam życzymy wszystkiego PANDAstycznego!
+Fundacja Panda Team
 ";
 
         var receiveConfirmationUrl = CreateReceiveConfirmationUrl(letter);
 
         var htmlContent = $@"<h1>Drodzy PANDAstyczni Darczyńcy!</h1>
-<p>Bardzo dziękujemy za Wasze zaangażowanie w akcję Panda Claus.</p>
-<p>Gratulujemy {letter.AssignedTo}! Potwierdzamy realizowanie wybranego przez Ciebie listu nr {letter.Number}. List znajdziesz pod tym adresem: <a href=\""{GetLetterUrl(letter)}\"">{GetLetterUrl(letter)}</a></p>
-<p>Jak wiecie, w tym roku mieliśmy rekordową liczbę zgłoszonych listów — dokładnie 240! Akcja z roku na rok rośnie i jest coraz większym wyzwaniem logistycznym. W związku z tym przypominamy najważniejsze zasady i dziękujemy Wam za wyrozumiałość:</p>
-<p>Paczki muszą być oznaczone numerami listów — to BARDZO ważne i pozwoli nam uniknąć pomyłek.</p>
-<p><b>Proszę o wysyłanie skompletowanych paczek. Tylko w taki sposób jesteśmy w stanie uniknąć pomyłek.</b> Bardzo złym pomysłem jest zamawianie zabawek bezpośrednio ze sklepu do nas i dosyłanie osobno innych upominków. To prowadzi do błędów i innych trudności organizacyjnych. Takie paczki nie będą niestety przyjmowane.</p>
-<p>Jeżeli realizujesz kilka listów, paczkę dla każdego dziecka spakuj osobno i umieść wszystkie możliwie w jednym kartonie.</p>
-<p>Do listu możesz dołączyć upominek dla rodziców dziecka, list lub kartkę oraz słodycze itd.</p>
-<p>W tym roku naszym Partnerem jest InPost, dlatego paczki muszą mieć rozmiary zgodne z wymaganiami <b>Paczkomatów InPost</b>.</p>
-<p><b>Paczkę wyślij Paczkomatem InPost</b> pod numer <b>POZ82M</b>. (jeżeli paczka nie zmieści się do Paczkomatu Inpost — daj nam znać na wyżej podany adres mailowy. Prosimy, <p>nie nadawaj</p> paczki kurierem bez informowania nas o tym)</p>
-<p>Nadając przesyłkę, podaj e-mail: <a href=""mailto:pandaclaus@pandateam.pl"">pandaclaus@pandateam.pl</a> oraz numer telefonu: +48 534 897 105.</p>
-<h2>Jeżeli chcesz dostarczyć paczkę osobiście:</h2>
-<p>Przywieź paczkę do naszego magazynu na ul. Głogowskiej 16 w Poznaniu (naprzeciwko Dworca Zachodniego, hala numer 2 MTP) w dniach:</p>
+<p>Bardzo dziękujemy za Wasze zaangażowanie w akcję <b>Panda Claus 2025.</b></p>
+<p>Gratulujemy {letter.AssignedTo}! Potwierdzamy realizowanie wybranego przez Ciebie listu nr {letter.Number}. List znajdziesz pod tym adresem: <a href=""{GetLetterUrl(letter)}"">{GetLetterUrl(letter)}</a></p>
+<p><b>Mamy absolutny rekord</b> - jak wiecie, w tym roku - trafiło do nas ponad 300 listów! To trzysta dziecięcych marzeń, trzysta historii, trzysta prezentów czekających na spełnienie.</p>
+<p>Akcja z roku na rok rośnie i jest coraz większym wyzwaniem logistycznym. W związku z tym przypominamy najważniejsze zasady i dziękujemy Wam za wyrozumiałość:</p>
 <ul>
-    <li>5-6 grudnia od 17:30 do 20:30</li>
-    <li>7 grudnia od 10:30 do 14:30</li>
+    <li><b>Na paczki czekamy do 6 grudnia!</b></li>
+    <li><b style=""color: red;"">Paczki muszą być oznaczone numerami listów</b> — to <b>BARDZO</b> ważne i pozwoli nam uniknąć pomyłek.</li>
+    <li>Prosimy Was także o <b style=""color: red;"">wydrukowanie (z załącznika) kodu QR</b> i przyklejenie go na paczce - ułatwi to naszym Magazynowym Elfom pracę i dzięki temu dostaniesz od nas <b>POTWIERDZENIE ODBIORU PACZKI!</b></li>
+    <li><b>Prosimy o wysyłanie skompletowanych paczek. Tylko w taki sposób jesteśmy w stanie uniknąć pomyłek.</b> Prosimy nie zamawiać zabawek bezpośrednio ze sklepu do nas i dosyłanie osobno innych upominków. To prowadzi do błędów i innych trudności organizacyjnych. Takie paczki nie będą niestety przyjmowane.</li>
+    <li>Prosimy nie pakować paczek w ozdobny papier - podczas finału akcji nasi wolontariusze sprawdzają każdą z nich i szykują prezenty do wysyłki :).</li>
+    <li>Zapraszamy do skorzystania z zasobów naszego Partnera - Biblioart - zachęcamy do sprawdzenia strony www - może to właśnie tam znajdziecie wymarzony prezent naszych podopiecznych: <a href=""https://bibliopunkt.pl/"" target=""_blank"">https://bibliopunkt.pl/</a></li>
+    <li>Jeżeli realizujesz kilka listów, paczkę dla każdego dziecka spakuj osobno i umieść wszystkie możliwie w jednym kartonie - opisując paczkę każdym z numerów listów, które realizujesz!</li>
+    <li>Do listu możesz dołączyć upominek dla rodziców dziecka, list lub kartkę oraz słodycze itd.</li>
+</ul>
+<p>Jak co roku naszym Partnerem jest <b>InPost</b>, dlatego paczki muszą mieć rozmiary zgodne z wymaganiami <b>Paczkomatów InPost</b> - Elfowie Kurierzy będą pracować w pocie czoła - pomóżmy im i trzymajmy się wymiarów paczkomatowych - mała ściąga tutaj:</p>
+<ul>
+    <li>Gabaryt A – maksymalne wymiary 8 × 38 × 64 cm, maksymalna waga: 25 kg</li>
+    <li>Gabaryt B – maksymalne wymiary 19 × 38 × 64 cm, maksymalna waga: 25 kg</li>
+    <li>Gabaryt C – maksymalne wymiary 41 × 38 × 64 cm, maksymalna waga: 25 kg</li>
+</ul>
+<p><b>Paczkę wyślij do nas pod adres:</b><br/>
+<b>Fundacja Panda Team</b><br/>
+<b>ul. Opłotki 23</b><br/>
+<b>60-012 Poznań</b></p>
+<p>Nadając przesyłkę, podaj e-mail: <a href=""mailto:pandaclaus@pandateam.pl"">pandaclaus@pandateam.pl</a> oraz numer telefonu: +48 537 925 335.</p>
+<h2>Jeżeli chcesz dostarczyć paczkę osobiście:</h2>
+<p><b style=""color: red;"">Poinformuj nas o tym do 6 grudnia!</b></p>
+<p>Przywieź paczkę do naszego magazynu w Poznaniu Hala numer 10 MTP (wejście od ulicy Śniadeckich) w dniu:</p>
+<ul>
+    <li>11 grudnia 2025 od godziny 16:00 do 20:30</li>
 </ul>
 <p>Z góry BARDZO dziękujemy za dostosowanie się do naszych szczegółowych wytycznych. Pomoże nam to sprawnie przeprowadzić akcję i dostarczyć prezenty do naszych podopiecznych :)</p>
-<p>Już teraz zapraszamy Was do zgłaszania się do wolontariatu podczas finału, który odbędzie się 13 i 14 grudnia (pt-sob) w hali nr 10 Międzynarodowych Targów Poznańskich. Chęć pomocy można zgłosić poprzez formularz dostępny na stronie <a href=""https://forms.gle/xn8UMCst3uSTwoeV9"" target=""_blank"">https://forms.gle/xn8UMCst3uSTwoeV9</a> (wolontariaty pracownicze proszę zgłaszać e-mailowo). Będzie nam miło, jak pomożecie nam w finale! :) </p>
+<p>Już teraz zapraszamy Was do zgłaszania się do wolontariatu podczas finału, który odbędzie się 12 i 13 grudnia (pt-sob) w hali nr 10 Międzynarodowych Targów Poznańskich. Chęć pomocy można zgłosić poprzez wysłanie wiadomości e-mail na adres: <a href=""mailto:wolontariat@pandateam.pl"">wolontariat@pandateam.pl</a> (wolontariaty pracownicze także prosimy zgłaszać e-mailowo). Będzie nam miło, jak pomożecie nam w finale! :)</p>
 <p>Z całym #pandateam życzymy wszystkiego PANDAstycznego!</p>
+<p><b>Fundacja Panda Team</b></p>
 <hr/>
 <h2>Potwierdzenie odbioru paczki</h2>
 <p><b>Wydrukuj załączony plik PDF i przyklej na wszystkie wysłane do nas paczki!</b></p>
